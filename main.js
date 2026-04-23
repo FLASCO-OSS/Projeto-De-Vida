8725ec06bb42b29c28c05e15081291b89d096f76
FLASCO-OSS <renatoakioh@gmail.com>
2026-04-23 17:54:01 -0300

    Update main.js

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2025-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2025-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2025-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2025-02-01T00:00:00");
+const tempoObjetivo1 = new Date("2025-10-05T100:59:59");
+const tempoObjetivo2 = new Date("2025-12-05T100:59:59");
+const tempoObjetivo3 = new Date("2025-12-30T100:59:59");
+const tempoObjetivo4 = new Date("2025-02-01T100:59:59");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
